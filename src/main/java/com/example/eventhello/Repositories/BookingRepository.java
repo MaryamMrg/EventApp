b882e925fc44a35c37c1b710dbeea8bbeb9346df
maryam mrg <maryamelouadaa@gmail.com>
2025-06-03 17:46:48 +0100

    add frontend and set up

--- src/main/java/com/example/eventhello/Repositories/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventhello.Repositories;
-
-import com.example.eventhello.entities.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
--- src/main/java/com/example/eventhello/Repositories/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventhello.Repositories;
-
-import com.example.eventhello.entities.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-
-    public Event findByTitle(String title);
-}
