c8f9a7a9a4d6189d787c17c2359ea4903bbed9b6
maryam mrg <maryamelouadaa@gmail.com>
2025-06-03 19:19:26 +0100

    the signup compenent with html css only

--- eventHello_frontend/src/app/app.ts
+++ eventHello_frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Home } from "./home/home";
+import { Signup } from "./signup/signup";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header, Home, Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
